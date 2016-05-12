89a8cfb016bb7fe23405b97256b491fd1d7903ab
Ivan.Orlov <ivan@morrr.com>
2016-05-12 09:45:45 +0300

    Removing router

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ var mod = require(__dirname + '/src/realm.js');
 
 mod.realm.transpiler = require(__dirname + '/src/transpiler.js');
 mod.realm.serve = require(__dirname + '/src/serve.js');
-mod.realm.router = require(__dirname + '/src/router/router.js');
 module.exports = mod.realm;
